acf678707bd90bd26d17c158ed55b98543abdf9f
ashin-krish <ashinkrish066@gmail.com>
2026-02-01 21:27:31 +0530

    updated the code to check using postman

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.post("/submit", (req, res) => {
     const { name, email, message } = req.body;
-
     console.log("Received:");
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Message:", message);
-
     res.send("Message received successfully!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({ status: "Backend is running!" });
+});
+
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
 });
 
 const PORT = process.env.PORT || 5000;
