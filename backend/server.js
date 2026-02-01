b81f89d47a5dbb284172ed4a5b9ce99cca4cc3f0
ashin-krish <ashinkrish066@gmail.com>
2026-02-01 20:48:17 +0530

    updated the code to deploy

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.post("/submit", (req, res) => {
     res.send("Message received successfully!");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
